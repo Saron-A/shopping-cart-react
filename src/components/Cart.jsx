e390dad6bcdea9ed83165d5a964298fb3c54910f
Saron <saronabebe2019@gmail.com>
2025-05-24 19:49:40 -0800

    Add go-back-to-products button

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Cart = ({ cart, setCart }) => {
   //in cart we have product and quantity, item represents the product and quantity, product represents id, name, price, etc. so if we want to display the product name, we can access it through item.name or
   return (
-    <div>
+    <div style={{}}>
       <h1>Your Cart</h1>
       {cart.length <= 0
         ? "Your cart is empty!"
         : cart.map((item) => (
-            <li>
+            <li key={item.id}>
               <div className="item">
                 <p>Number of items: {item.quantity}</p>
                 <h2>Item(s): {item.title}</h2>
@@ -18,7 +18,21 @@ const Cart = ({ cart, setCart }) => {
             </li>
           ))}
 
-      <Link to="/">Go back to Products</Link>
+      <Link
+        to="/"
+        style={{
+          position: "fixed",
+          bottom: "2rem",
+          right: "1rem",
+          border: "1px solid black",
+          borderRadius: "1rem",
+          padding: "0.5rem",
+          textDecoration: "none",
+          backgroundColor: "#f0f0f0",
+        }}
+      >
+        Go back to Products
+      </Link>
     </div>
   );
 };
