09ff5255554ca1db4af8fa2ecdc772e4bc994157
Saron <saronabebe2019@gmail.com>
2025-05-28 13:10:16 -0800

    Add CSS to checkout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,34 +5,29 @@ import "../index.css"; // Import your CSS styles
 const Checkout = () => {
   const { cart } = useOutletContext();
   return (
-    <div>
+    <div className="checkout-container">
       <h1>Cart Checkout Page</h1>
       <div className="checkout">
-        <h1>Items: </h1>
-        <ul>
+        <h1>Items in your cart: </h1>
+        <ul className="checkout-list">
           {" "}
           {cart.map((item) => (
             <li key={item.id}>
-              <ul
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "1rem",
-                }}
-              >
+              <ul className="checkout-item">
                 <li> {item.title}</li>
-                <li>Quantity: {item.quantity}</li>
-                <li>Price: {item.price * item.quantity}$</li>
+                <div className="sub-item">
+                  <li>Quantity: {item.quantity}</li>
+                  <li>Price: {item.price * item.quantity}$</li>
+                </div>
               </ul>
             </li>
           ))}
         </ul>
         <h2>
           Total Price:{" "}
-          {cart.reduce(
-            (total, item) => (total += item.price * item.quantity),
-            0
-          )}
+          {cart
+            .reduce((total, item) => (total += item.price * item.quantity), 0)
+            .toFixed(2)}
           $
         </h2>
       </div>
